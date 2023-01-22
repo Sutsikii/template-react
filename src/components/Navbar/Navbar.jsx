65bde3f965286530dc6944146a3b1d09ea6ba167
Sutsikii <nicolas-dutertre@bbox.fr>
2023-01-22 20:13:48 +0100

    Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <navbar class="navbar">
+
+      <a href="/" class="logo">Sutsiki</a>
+      <div class="nav-links">
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Projet</a></li>
+          <li><a href="#">Infos</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+
+      <header></header>
+
+    </navbar>
+  )
+}
+
+export default Navbar
